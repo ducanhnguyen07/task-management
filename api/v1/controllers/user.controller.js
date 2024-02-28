a9a5aeb6325de920f00ed5ae5b6ba19c3105d0ca
Nguyễn Đức Anh <ducanhnguyend21cntt@gmail.com>
2024-02-28 20:11:55 +0700

    6. Lưu những người tham gia vào task

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -198,3 +198,17 @@ module.exports.detail = async (req, res) => {
     info: req.user
   });
 };
+
+//  [GET] /api/v1/user/list
+module.exports.list = async (req, res) => {
+  
+  const users = await User.find({
+    deleted: false
+  }).select("fullName email");
+
+  res.json({
+    code: 200,
+    message: "Thành công!",
+    users: users
+  });
+};
