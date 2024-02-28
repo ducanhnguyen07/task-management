5ca0d6f5a60745fb803aee930f41c6d48035e850
Nguyễn Đức Anh <ducanhnguyend21cntt@gmail.com>
2024-02-28 16:20:24 +0700

    3. Trang thông tin cá nhân

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -102,7 +102,7 @@ module.exports.forgotPassword = async (req, res) => {
   const objectForgotPassword = {
     email: email,
     otp: otp,
-    expireAt: Date.now() + timeExpire * 60
+    expireAt: Date.now() + timeExpire * 60 * 1000
   };
 
   // save into db
@@ -187,5 +187,21 @@ module.exports.resetPassword = async (req, res) => {
   res.json({
     code: 200,
     message: "Đổi mật khẩu thành công!"
+  });
+};
+
+//  [GET] /api/v1/user/detail/:id
+module.exports.detail = async (req, res) => {
+  const id = req.params.id;
+
+  const user = await User.findOne({
+    _id: id,
+    deleted: false
+  }).select("-password -token");
+  
+  res.json({
+    code: 200,
+    message: "Thành công!",
+    info: user
   });
 };
